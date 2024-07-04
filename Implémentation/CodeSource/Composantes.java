cc13d0720a433bbfd1121191ded8e71ecb2b3c5b
Hicham <121328301+hcmbn7@users.noreply.github.com>
2024-07-04 15:14:39 -0400

    modif classes Composantes, robot et CPU

--- Implémentation/CodeSource/Composantes.java
+++ Implémentation/CodeSource/Composantes.java
@@ -1,7 +1,56 @@
 public class Composantes {
     private String nom;
     private String description;
-    private Float prix;
+    private Double prix;
     private Boolean isDisponible;
     private ComposanteType type;
+
+
+        public String getNom() {
+            return nom;
+        }
+    
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+    
+
+        public String getDescription() {
+            return description;
+        }
+    
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    
+
+        public Double getPrix() {
+            return prix;
+        }
+    
+
+        public void setPrix(Double prix) {
+            this.prix = prix;
+        }
+    
+
+        public Boolean getIsDisponible() {
+            return isDisponible;
+        }
+    
+
+        public void setIsDisponible(Boolean isDisponible) {
+            this.isDisponible = isDisponible;
+        }
+
+        public ComposanteType getType() {
+            return type;
+        }
+    
+
+        public void setType(ComposanteType type) {
+            this.type = type;
+        }
 }
--- Implémentation/CodeSource/Robot.java
+++ Implémentation/CodeSource/Robot.java
@@ -1,7 +1,66 @@
+import java.util.ArrayList;
+
 public class Robot {
-    private Integer numeroDeSerie;
+    private CPU cpu;
     private String nom;
     private String type;
+    private ArrayList<Composantes> composantesRobot;
+
+    public Robot(CPU cpu, String nom, String type) {
+        this.cpu = cpu;
+        this.nom = nom;
+        this.type = type;
+        this.composantesRobot = new ArrayList<>();
+    }
+
+    public Integer getnumeroDeSerie(){
+        return cpu.getNumeroDeSerie();
+    }
+
+    public CPU getCpu() {
+        return cpu;
+    }
+
+
+    public void setCpu(CPU cpu) {
+        this.cpu = cpu;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<Composantes> getComposantesRobot() {
+        return composantesRobot;
+    }
+
+    public void setComposantesRobot(ArrayList<Composantes> composantesRobot) {
+        this.composantesRobot = composantesRobot;
+    }
+
+    public void addComposante(Composantes composante) {
+        this.composantesRobot.add(composante);
+    }
+
+    public void removeComposante(Composantes composante) {
+        this.composantesRobot.remove(composante);
+    }
 
     public String afficherEtat(){
         return "";
